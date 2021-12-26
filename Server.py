1566c2938f5dc46d342686299ede2aee35723824
Sbailea795 <Sbailea795@outlook.com>
2021-12-26 13:28:01 -0600

    Functional/Feature Complete, refactoring / cleanup still needed

--- Server.py
+++ Server.py
@@ -2,38 +2,41 @@
 import os
 import socket                                         
 import time
-
-clientsocket = socket.socket()
-
+cls = '\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n'
 class Main:
 
     def loop(self):
 
-        # create a socket object
-        serversocket = socket.socket(
-                    socket.AF_INET, socket.SOCK_STREAM) 
-
-        # get local machine name
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         host = socket.gethostname()                        
-
-        port = 2115                                           
-
-        # bind to the port
+        port = 2115
         serversocket.bind(('', port))
 
         # queue up to 5 requests
         serversocket.listen(5)                                           
 
         while True:
-            # establish a connection
-            clientsocket,addr = serversocket.accept()      
+            clientsocket,addr = serversocket.accept()  
             data = ''
             print("Got a connection from %s" % str(addr))
-            #msg = serversocket.recv(1024)
-            #print (msg.decode('utf-8'))
+
+            clientsocket.send('This Is meant for a ratio of (COL:168, ROW:34) character displays.\r\n'.encode('utf-8'))
+            clientsocket.send('This is designed for UTF-8 chacters, not all terminals/telnet applications support this. Please consider using PuTTY or another UTF-8 capaable telnet client.\r\n\r\nTo continue with ASCII encoding, press 1 (not recommended), otherwise press any key + enter (recommended)\r\n'.encode('utf-8'))
+            #time.sleep(5)
+            isASCII = False
+            try:
+                print('try')
+                clientsocket.settimeout(None)
+                msg = clientsocket.recv(1024)
+                msg = clientsocket.recv(1024).strip()
+                if msg.decode('utf-8') == '1': 
+                    isASCII = True
+                print('again?')
+            except:
+                continue
             clientsocket.send(data.encode('utf-8'))
             print('welcome')
-            Main.welcome()
+            Main.welcome(clientsocket)
             print('movie starting')
             begin = time.time()
             for segment in range(0,7304):
@@ -47,30 +50,23 @@ class Main:
                         textfile.readline()
                     for line in range(0, 33):
                         data += textfile.readline()
-                    data = '\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n' + data.replace('\n', '\r\n')
-                    
-                    #data = data.replace('█', '#')
-                    #data = data.replace('▓', '%')
-                    #data = data.replace('▒', '|')
-                    #data = data.replace('░', '!')
-                    #data = data.replace('╬', '^')
-                    #data = data.replace('┼', '=')
-                    #data = data.replace('□', '-')
-                    #data = data.replace('▫', '.')
+                    data = cls + data.replace('\n', '\r\n')
+                    if isASCII:
+                        data = data.replace('█', '#')
+                        data = data.replace('▓', '%')
+                        data = data.replace('▒', '|')
+                        data = data.replace('░', '!')
+                        data = data.replace('╬', '^')
+                        data = data.replace('┼', '=')
+                        data = data.replace('□', '-')
+                        data = data.replace('▫', '.')
                     
                     #for line in range(0, frame * 18):
                         #subfile.readline()
                     #for line in range(0, 18):
                         #byte += subfile.readline()
-                    #cls(self)
-                    #print('\n\n\n\n\n\n\n\n\n')
-                    #print('\n\n\n\n\n\n\n\n\n')
-                    #print('\n\n\n\n\n\n\n\n\n')
                     clientsocket.send(data.encode('utf-8'))
-                    #byte = textfile.readline()
-                    #byte = subfile.readline()
                     clientsocket.send(("Frame: {:9s} ".format(str(8*segment + frame)) + "Time Stamp: {:9s} ".format(str(segment + frame / 8)) + "Overhead Time: {:01.8f} ".format(time.time()-last_time) + "Time: {:6s} ".format(str(time.time()-begin))).encode())
-                    #clientsocket.send('\n\n\n\n\n\n\n\n\n'.encode('utf-8'))
                     if time.time() - begin <= segment:
                         time.sleep(4/16)
                     elif time.time() - begin <= segment + 1/4:
@@ -81,25 +77,22 @@ class Main:
                         time.sleep(1/16)
             clientsocket.close()
 
-    def welcome():
+    def welcome(clientSocket):
         text = ''
         textfile = open('./MovieSegments/FrameOutline.txt', 'r', encoding='utf-8')
-        while (textfile.readable): 
-            text = textfile.readline
-        clientsocket.send(text.encode('utf-8'))
-        print('test1')
-        time.sleep(3)
-        print('test1a')
+        for line in range(0, 34):
+            text += textfile.readline()
+        text = cls + text.replace('\n', '\r\n')
+        clientSocket.send(text.encode('utf-8'))
+        time.sleep(5)
+        
         text = ''
         textfile = open('./MovieSegments/TitleCard.txt', 'r', encoding='utf-8')
-        while (textfile.readable): 
-            text = textfile.readline
-        clientsocket.send(text.encode('utf-8'))
-        time.sleep(3)
-        print('test2')
-
-#def cls(self):
-    #os.system('cls' if os.name == 'nt' else 'clear')
+        for line in range(0, 34):
+            text += textfile.readline()
+        text = cls + text.replace('\n', '\r\n')
+        clientSocket.send(text.encode('utf-8'))
+        time.sleep(10)
 
 Object = Main()
 Object.loop()
